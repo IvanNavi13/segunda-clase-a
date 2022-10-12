89f56dc2e86a311a15b6f1db0ba0f62a6ba2784f
IvanNavi13 <ivannavi.13@hotmail.com>
2022-10-11 20:28:59 -0500

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RequestService } from './services/request/request.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'segunda-clase-a';
+  name: string = '';
+  order: string = '';
+
+  constructor( public requestService: RequestService){}
+
+  ngOnInit(): void {
+    // this.requestService.getPokemon('pikachu');
+    this.funcionBotonHttp();
+  }
+
+  funcionBotonHttp(){
+    let buton = document.getElementById('search');
+    let inName: HTMLInputElement = document.getElementById('inName') as HTMLInputElement;
+
+    console.log(inName);
+    buton?.addEventListener('click', () => {
+      this.requestService.getPokemon(inName.value).subscribe({
+        next : (resp: any) => {
+          
+          this.name = resp.name;
+          this.order = resp.order;
+          console.log("component: ",resp);
+
+        },
+        error: (err : any ) => {
+          this.name = "error";
+          this.order = "error";
+           
+        }
+      });
+    })
+  }
+
 }
